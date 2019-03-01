73af6136de6924444a5f234296eb77e5397b1ed1
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-01 09:25:10 +0100

    Added functionality to add smurfs to the database using POST

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -14,10 +15,23 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios.get('http://localhost:3333/smurfs')
+      .then(res => this.setState({smurfs: res.data}))
+      .catch(err => console.log('Somethings wrong', err));
+  }
+
+  postSmurf = (name, age, height) => {
+    axios.post('http://localhost:3333/smurfs', {name, age, height})
+      .then(res => this.setState({smurfs: res.data}))
+      .catch(err => console.log(`Something's wrong with the POSTSMURF() ${err}`));
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm postSmurf={this.postSmurf} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
